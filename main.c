3cb87b40ddffc81b7053ce7ba44995fe31ad234e
Temmiebecca <temmiebecca@gmail.com>
2023-10-22 21:48:56 -0700

    updated

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+stack_t *head = NULL;
 
 
 /**
@@ -10,8 +11,6 @@
 
 int main(int argc, char *argv[])
 {
-	stack_t *head = NULL;
-
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
